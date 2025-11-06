d7b5b75df3fd1d7774f9653b4d635ffd4b03b3cb
Max Enfayz <ashenarmor558@gmail.com>
2025-11-06 18:07:53 +0000

    feat: Add orientation prop to Separator component

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -17,6 +17,8 @@ function Separator({
         data-slot="separator"
         decorative={decorative}
         orientation={orientation}
+        role="separator"
+        aria-orientation={orientation}
         className={cn("bg-border h-px w-full shrink-0", className)}
         {...props}
       />
@@ -28,6 +30,8 @@ function Separator({
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
+      role="separator"
+      aria-orientation={orientation}
       className={cn(
         "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
         className
