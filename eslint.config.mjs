de5e5fb208860b91b8ca2589a129f8cd6b40a419
Ebba Alva <ebba.alva77@gmail.com>
2025-11-28 09:54:05 -0700

    feat: Implement Changelog Page with Filtering and Deep Linking (#70)
    * feat: Implement changelog page with filtering and deep linking
    
    * Fix changelog sorting and client component declaration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,12 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { FlatCompat } from '@eslint/eslintrc';
+import js from "@eslint/js";
 
 import eslintPluginPrettier from "eslint-plugin-prettier";
-import nextPlugin from "eslint-plugin-next";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextPlugin from "@next/eslint-plugin-next";
 
 const eslintConfig = [
+  js.configs.recommended,
   nextPlugin.configs.recommended,
   nextPlugin.configs["core-web-vitals"],
-  ...compat.extends("prettier"),
   {
     ignores: [
       "node_modules/**",
@@ -34,3 +25,5 @@ const eslintConfig = [
     },
   },
 ];
+
+export default eslintConfig;
